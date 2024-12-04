0c568c10462be79579a3b300c7229752a3a77feb
Neil Jara <81881049+NeilDevop@users.noreply.github.com>
2024-12-03 22:59:08 -0300

    Refactor dashboard.js: Comment out opening of dev tools

--- app/manager/dashboard.js
+++ app/manager/dashboard.js
@@ -28,7 +28,7 @@ function createDashboardView(mainWindow) {
     },
   });
   mainWindow.contentView.addChildView(browserView);
-  browserView.webContents.openDevTools();
+  //browserView.webContents.openDevTools();
   browserView.webContents.loadURL('https://www.google.com');
 
   frameView = new WebContentsView({
