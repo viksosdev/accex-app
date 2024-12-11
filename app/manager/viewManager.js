81c284fbb64f29a65b4b95275d069461dfd4e83c
Camilo Lagos I <67244497+kmilol4gos@users.noreply.github.com>
2024-12-11 15:18:29 -0300

    Add chatbot view and integrate into dashboard layout; update view management

--- app/manager/viewManager.js
+++ app/manager/viewManager.js
@@ -1,31 +1,40 @@
-function adjustView(mainWindow, sidebarView, browserView, frameView) {
-  const { width, height } = mainWindow.getBounds();
+function adjustView(mainWindow, sidebarView, browserView, frameView, botView) {
+	const { width, height } = mainWindow.getBounds();
 
-  const sidebarWidth = 75;
-  const frameHeight = 75;
+	const sidebarWidth = 75;
+	const frameHeight = 75;
+	const botHeight = 436;
+	const botX = 90;
 
-  sidebarView.setBounds({
-    x: 0,
-    y: frameHeight,
-    width: sidebarWidth,
-    height,
-  });
+	sidebarView.setBounds({
+		x: 0,
+		y: frameHeight,
+		width: sidebarWidth,
+		height,
+	});
 
-  browserView.setBounds({
-    x: sidebarWidth,
-    y: frameHeight,
-    width: width - sidebarWidth,
-    height: height - frameHeight - 30,
-  });
+	browserView.setBounds({
+		x: sidebarWidth,
+		y: frameHeight,
+		width: width - sidebarWidth,
+		height: height - frameHeight - 30,
+	});
 
-  frameView.setBounds({
-    x: 0,
-    y: 0,
-    width: width,
-    height: frameHeight,
-  });
+	frameView.setBounds({
+		x: 0,
+		y: 0,
+		width: width,
+		height: frameHeight,
+	});
+
+	botView.setBounds({
+		x: botX,
+		y: height - 480,
+		width: 329,
+		height: botHeight,
+	});
 }
 
 module.exports = {
-  adjustView,
+	adjustView,
 };
