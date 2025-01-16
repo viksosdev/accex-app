e43c3a418fa09bc5f5673e7670cc2ab26301d99e
Victor U. Ramirez <benjamin@alphanex.cl>
2025-01-16 13:28:29 -0300

    Arreglo visual en registro

--- app/renderer/scripts/renderer.js
+++ app/renderer/scripts/renderer.js
@@ -1,18 +1,18 @@
-document.getElementById('searchButton').addEventListener('click', () => {
-  const query = document.getElementById('searchInput').value;
+document.getElementById("searchButton").addEventListener("click", () => {
+  const query = document.getElementById("searchInput").value;
   window.api.sendSearchQuery(query); // Enviar la consulta al proceso principal
 });
 
 // Escuchar los resultados de la búsqueda
 window.api.onSearchResult((result) => {
-  const resultDiv = document.getElementById('searchResults');
+  const resultDiv = document.getElementById("searchResults");
   resultDiv.innerHTML = result; // Mostrar los resultados en el DOM
 });
 
 // Mostrar indicadores de carga
 window.api.onLoadingStarted(() => {
-  document.getElementById('loadingIndicator').style.display = 'block';
+  document.getElementById("loadingIndicator").style.display = "block";
 });
 window.api.onLoadingStopped(() => {
-  document.getElementById('loadingIndicator').style.display = 'none';
+  document.getElementById("loadingIndicator").style.display = "none";
 });
