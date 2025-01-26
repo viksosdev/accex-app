0cc1163e58944b2b4781e5cd2dbd61019664c966
Neil Jara <81881049+NeilDevop@users.noreply.github.com>
2025-01-26 00:22:05 -0300

    button config dasboard

--- app/renderer/scripts/sidebar.js
+++ app/renderer/scripts/sidebar.js
@@ -1,5 +1,10 @@
 const botButton = document.getElementById("bot");
+const configButton = document.getElementById("config-button");
 
 botButton.addEventListener("click", () => {
   window.electronAPI.send("open-bot");
+});
+
+configButton.addEventListener("click", () => {
+  window.appNavigate.go('load-registerConfig-page');
 });
